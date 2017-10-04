d49e12cc0e1f1a134043e3657556def36646c3c2
hyojung <gmolla>
2017-10-04 21:09:45 +0900

    AlbumViewController 추가, 오류수정해야함
    AlbumViewController에서 RecordViewController로 넘어갈때 lati/longi 변수에대해서 "unexpectedly found nil while unwrapping an Optional value"오류 발생..ㅜ

---
+++ memoApp/memo/Record/AlbumViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AlbumViewController.swift
+//  memo
+//
+//  Created by 이효중 on 2017. 10. 4..
+//  Copyright © 2017년 이효중. All rights reserved.
+//
+/* 참고
+ https://m.blog.naver.com/PostView.nhn?blogId=scw0531&logNo=220848324272&proxyReferer=https%3A%2F%2Fwww.google.co.kr%2F
+ */
+
+import UIKit
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
