6c507fa94abd58d1a4514115d7f131cc63c5bf08
ryudonghun78 <ryudonghun78@gmail.com>
2017-11-05 18:28:40 +0900

    이미지를 세그로 전달 및 오류 제거
    RecordViewController내의 imageView로 AlbumViewController에서 선택한 이미지를 전달하는
    것까지 성공
    RecordViewController를 destination으로 설정한 후
    destination.imageViewSegue.image = imageForSeuge라고 시도 했지만 성공하지 못함. 계속
    nil값이 들어갔다고 해서 해결책을 찾음
    RecordViewController에 image를 저장할 UIImage를 만들고 그 image에 저장 한 후
    RecordViewController부분의 viewDidLoad()부분에서
    실제 이미지가 세그를 통해 전달이 되었다면 이미지 뷰에 저장하는 방식으로 해결했다.

--- memoApp/memo/Record/AlbumViewController.swift
+++ memoApp/memo/Record/AlbumViewController.swift
@@ -16,6 +16,9 @@ class AlbumViewController: UIViewController, UIImagePickerControllerDelegate, UI
     //이미지 뷰어를 통해 만들어 두었다.
     @IBOutlet weak var imageView: UIImageView!
     
+    //세그를 위한 이미지 저장 변수
+    var imageForSegue : UIImage!;
+    
     //이미지를 선택했을때 작동하는 함수 부분
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         print("info : \(info)")
@@ -23,6 +26,10 @@ class AlbumViewController: UIViewController, UIImagePickerControllerDelegate, UI
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         //이미지 뷰어에 실제로 이미지를 저장하는 과정. 여기서 저장한 변수를 통해 다른 세그로 날릴 수 있을 것 같다.
         imageView.image = image;
+        
+        //세그 전달을 위해 이미지 저장
+        imageForSegue = image;
+        
         print("Success image Save...");
         picker.dismiss(animated: true, completion: nil);
         print("dismiss success");
@@ -55,6 +62,44 @@ class AlbumViewController: UIViewController, UIImagePickerControllerDelegate, UI
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //세그 전달을 위한 함수 ( 준비시키는 과정 )
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("enter prepare...")
+        //전달하고자 하는 세그가 imageSegue에 해당한다면
+        if segue.identifier == "imageSegue"{
+            //레코드 뷰를 destination으로 설정한다.
+            if let destination = segue.destination as? RecordViewController{
+//                print("enter segue prepare...")
+//                print(imageForSegue)
+                //RecordViewController로 이미지를 segue를 통해서 전달하는데 성공.
+                destination.imageForSegue = imageForSegue
+//                print("success segue prepare...")
+                
+                //근데 항상 실행을 할 때 이 부분으로 2번 접근한다
+                //왜그런지는 해결하지 못함 아직
+            }
+        }
+        
+
+        //확인 주석
+        print("sucess to save image and send RecordViewController...")
+    }
+    
+    //선택 버튼을 눌렀을때 처리할 코드
+    @IBAction func saveBtn(_ sender: Any) {
+        
+        imageForSegue = imageView.image
+        //이미지가 현재 전달 될 준비가 되어있다면
+        if imageForSegue != nil{
+            //세그를 실행한다.
+            performSegue(withIdentifier: "imageSegue", sender: self)
+        }
+        
+    }
+    
+    
+    
     
 
     /*
