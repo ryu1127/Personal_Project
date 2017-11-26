c32f8d3b6fde3adc24f2d60a3b699abc6f3e24a7
ryudonghun78 <ryudonghun78@gmail.com>
2017-11-26 21:53:46 +0900

    facebook로그인 마무리
    마무리는 하였으나 실제적인 성공은 하지 못함. 로그인 한 후 다음 화면 넘어가는 것 까지 성공하고 로그인에 성공하고 그 이메일 정보와 id토큰, 이름까지 가져왔으나 FIRAuth부분에서 인식을 못하는 것 같음.
    오류를 일주일 내내 찾으며 해결하려고 하였으나 최근 글은 없고 그 당시에는 FIRAuth자체 오류라서 새로운 SDK를 기다리라는 등 답변이 엇갈려 결국 해결하지 못했음.

--- memoApp/memo/AppDelegate.swift
+++ memoApp/memo/AppDelegate.swift
@@ -61,6 +61,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
 //    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
 //        return GIDSignIn.sharedInstance().handle(url, sourceApplication: sourceApplication, annotation: annotation)
 //    }
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError?){
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        return
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the applicaion is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- memoApp/memo/LoginViewController.swift
+++ memoApp/memo/LoginViewController.swift
@@ -11,7 +11,10 @@ import Firebase
 import GoogleSignIn
 import FBSDKLoginKit
 
+
 class LoginViewController: UIViewController/* , GIDSignInUIDelegate*/, FBSDKLoginButtonDelegate{
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,20 +36,30 @@ class LoginViewController: UIViewController/* , GIDSignInUIDelegate*/, FBSDKLogi
 //***** end of Google Sign In *****//
         
         
-        
 //***** Facebook Log In *****//
         let loginButton = FBSDKLoginButton()
         view.addSubview(loginButton)
         //frame's are obselete, please use constraints instead because its 2016 after all
         loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
         loginButton.delegate = self
+        loginButton.readPermissions = ["email","public_profile"] 
         print("success button make")
         
         
 //***** End of Facebook Log In *****//
         
-        
     }
+    
+    func handleCustomFBLogin(){
+        FBSDKLoginManager().logIn(withReadPermissions: ["email"], from: self) {(result,err) in
+            if err != nil{
+                print("Custom FB Login failed: ",err)
+                return
+            }
+            self.showEmailAddress()
+        }
+    }
+    
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
         print("Did log out of facebook....")
     }
@@ -56,18 +69,39 @@ class LoginViewController: UIViewController/* , GIDSignInUIDelegate*/, FBSDKLogi
             return
         }
         //segue를 만들어서 로그인 끝나고 다시 돌아올때 바로 다음화면으로 넘어갈 수 있게 만듬
-
+        
+        
         performSegue(withIdentifier: "facebookLogin", sender: self)
+        showEmailAddress()
         print("Successfully logged in with facebook...")
         
-    }
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    func showEmailAddress(){
+        let accessToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accessToken?.tokenString else {return}
+        let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        //이부분에서 에러가 남. 어떤 에러인지 스택오버플로우를 통해 검색해봤으나 해결하지 못함. 나온 방법들을 그대로 사용했으나 버전이 달라 해결 불가. 일단 다음으로 넘김
+        FIRAuth.auth()?.signIn(with: credentials, completion: {(user,err) in
+            if err != nil {
+                print("something went wrong with our FB user: ",err)
+                return
+            }
+            print("Successfully logged in with our user: ", user)
+        })
+        FBSDKGraphRequest(graphPath: "/me", parameters : ["fields":"id, name, email"]).start { (connection, result, err) in
+            if err != nil {
+                print("failed to start graph request: ", err ?? "")
+                return
+            }
+            print(result ?? "")
+        }
+    }
     /*
     // MARK: - Navigation
 
